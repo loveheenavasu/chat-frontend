61513e280f51204be03c08ff3b209eea3346ce4d
Aastha Kalra <aastha.js@zestgeek.com>
2024-08-20 13:17:37 +0530

    updated css of chatbotLink

--- src/app/admin/chat/chatbotLink/page.tsx
+++ src/app/admin/chat/chatbotLink/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import ChatbotLink from "@/components/admin/ChatbotLink";
 import SideBar from "@/components/admin/SideBar";
+// import UserData from "@/components/admin/UserData";
 import Header from "@/components/common/Header";
 import { getLocalStorageItem } from "@/utils/localStorage";
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
@@ -20,54 +21,43 @@ const page = () => {
 
   return (
     <>
-      {documentID ? (
-        <>
-          <Header />
-          <Box p="5px" border="1px solid #fff" w="fit-content">
-            <Link href="/">
-              <Flex
-                m="5"
-                p="10px"
-                alignItems="center"
-                cursor="pointer"
-                gap="1"
-                _hover={{ bg: "blue.50" }}
-              >
-                <IoMdArrowBack size="24px" /> Back
-              </Flex>
-            </Link>
-          </Box>
-
-          <Flex justifyContent="space-between" px="14" gap="3">
-            <Box w="20%">
-              <SideBar />
-            </Box>
-
-            <Box height="100vh" w="60%">
-              <ChatbotLink />
-            </Box>
-          </Flex>
-        </>
-      ) : (
-        <>
+      <Header />
+      <Box p="5px" border="1px solid #fff" w="fit-content">
+        <Link href="/">
           <Flex
-            justifyContent="center"
+            m="5"
+            p="10px"
             alignItems="center"
-            direction="column"
-            w="100%"
-            h="100vh"
+            cursor="pointer"
+            gap="1"
+            _hover={{ bg: "blue.50" }}
           >
+            <IoMdArrowBack size="24px" /> Back
+          </Flex>
+        </Link>
+      </Box>
+
+      <Flex justifyContent="space-between" px="14" gap="3">
+        <Box w="20%">
+          <SideBar />
+        </Box>
+
+        {/* <Box w="100%" mx="10">
+              <UserData />
+            </Box> */}
+
+        {documentID ? (
+          <Box height="100vh" w="60%">
+            <ChatbotLink />
+          </Box>
+        ) : (
+          <Flex justifyContent="center" w="100%">
             <Text as="b" fontSize="24" my="6">
               Please select any file or add text to generate a link.
             </Text>
-            <Link href="/admin/chat">
-              <Button bg="#5188b9" color="white" _hover={{}}>
-                Go Back
-              </Button>
-            </Link>
           </Flex>
-        </>
-      )}
+        )}
+      </Flex>
     </>
   );
 };
