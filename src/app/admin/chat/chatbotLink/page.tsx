19cf783364f29b01687756afe979838a80759f30
Aastha Kalra <aastha.js@zestgeek.com>
2024-08-20 13:11:32 +0530

    updated css of chatbotLink

--- src/app/admin/chat/chatbotLink/page.tsx
+++ src/app/admin/chat/chatbotLink/page.tsx
@@ -21,58 +21,43 @@ const page = () => {
 
   return (
     <>
-      {documentID ? (
-        <>
-          <Header />
-          <Box p="5px" border="1px solid #fff" w="fit-content">
-            <Link href="/">
-              <Flex
-                m="5"
-                p="10px"
-                alignItems="center"
-                cursor="pointer"
-                gap="1"
-                _hover={{ bg: "blue.50" }}
-              >
-                <IoMdArrowBack size="24px" /> Back
-              </Flex>
-            </Link>
-          </Box>
+      <Header />
+      <Box p="5px" border="1px solid #fff" w="fit-content">
+        <Link href="/">
+          <Flex
+            m="5"
+            p="10px"
+            alignItems="center"
+            cursor="pointer"
+            gap="1"
+            _hover={{ bg: "blue.50" }}
+          >
+            <IoMdArrowBack size="24px" /> Back
+          </Flex>
+        </Link>
+      </Box>
 
-          <Flex justifyContent="space-between" px="14" gap="3">
-            <Box w="20%">
-              <SideBar />
-            </Box>
+      <Flex justifyContent="space-between" px="14" gap="3">
+        <Box w="20%">
+          <SideBar />
+        </Box>
 
-            {/* <Box w="100%" mx="10">
+        {/* <Box w="100%" mx="10">
               <UserData />
             </Box> */}
 
-            <Box height="100vh" w="60%">
-              <ChatbotLink />
-            </Box>
-          </Flex>
-        </>
-      ) : (
-        <>
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            direction="column"
-            w="100%"
-            h="100vh"
-          >
+        {documentID ? (
+          <Box height="100vh" w="60%">
+            <ChatbotLink />
+          </Box>
+        ) : (
+          <Flex justifyContent="center" w="100%">
             <Text as="b" fontSize="24" my="6">
               Please select any file or add text to generate a link.
             </Text>
-            <Link href="/admin/chat">
-              <Button bg="#5188b9" color="white" _hover={{}}>
-                Go Back
-              </Button>
-            </Link>
           </Flex>
-        </>
-      )}
+        )}
+      </Flex>
     </>
   );
 };
