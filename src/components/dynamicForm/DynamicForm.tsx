7c1f2aa0aaaa64b09c420f6b17dca266cc40a7db
Aastha Kalra <aastha.js@zestgeek.com>
2024-09-03 17:57:22 +0530

    fixed  socket reconnect issue

--- src/components/dynamicForm/DynamicForm.tsx
+++ src/components/dynamicForm/DynamicForm.tsx
@@ -253,7 +253,7 @@ const DynamicForm = () => {
         }
       }
     } catch (error: any) {
-      toast.error(error.response.data.message);
+      toast.error(error?.response?.data?.message);
     } finally {
       setIsLoading(false);
     }
