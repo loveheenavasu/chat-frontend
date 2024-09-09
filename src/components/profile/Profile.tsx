c963454605899af05bf0a80571fd52fe3c69c475
zestgeek 12s <softwaredev@zestgeek.com>
2024-09-09 12:14:22 +0530

    Add profile menu functionality

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -3,14 +3,34 @@ import { removeLocalStorageItem } from "@/utils/localStorage";
 import { Button, Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList, Text } from "@chakra-ui/react"
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { CgProfile } from "react-icons/cg";
+interface ProfileData {
+    email: '';
+    firstname: '';
+    lastname: '';
 
-
-
+}
 const Profile = () => {
-
+    const [profileData, setProfiledata] = useState<ProfileData>()
+    console.log(profileData?.email, 'profileData')
     const router = useRouter()
+
+    const Aboutprofile = async () => {
+        try {
+            const response = await axiosInstance.get(`user/profile`);
+            console.log(response.data, 'responseee')
+            if (response?.data) {
+                setProfiledata(response.data)
+            }
+        } catch (error) {
+            toast.error("something went wrong");
+        }
+    }
+    useEffect(() => {
+        Aboutprofile()
+    }, [])
+
     const handleLogout = async () => {
         try {
             const response = await axiosInstance.delete(`user/logout`);
@@ -25,15 +45,14 @@ const Profile = () => {
         }
     };
 
-
     return (
         <Menu size={'sm'}>
             <MenuButton as={Button} borderRadius={'50px'} colorScheme={'blue'} textColor={'#fff'} mt={'-5px'}>
-                P
+                {profileData?.firstname.charAt(0)}
             </MenuButton>
             <MenuList p={'10px'}>
-                <MenuGroup textColor={'black'} title='Paras Sharma'>
-                    <MenuGroup mt={'-12px'} fontSize={'13px'} textColor={'grey'} fontWeight={'600'} title="paras.js@zestgeek.com"></MenuGroup>
+                <MenuGroup textColor={'black'} title={`${profileData?.firstname} ${profileData?.lastname}`}>
+                    <MenuGroup mt={'-12px'} fontSize={'13px'} textColor={'grey'} fontWeight={'600'} title={profileData?.email}></MenuGroup>
                     <MenuDivider />
                     <MenuItem textColor={'grey'}>Dashboard </MenuItem>
                     <MenuItem textColor={'grey'}>Account Settings </MenuItem>
