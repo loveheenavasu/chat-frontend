22d856b4fe883a88a93cb0dd499f982d4ec690e7
loveheenavasu <55981820+loveheenavasu@users.noreply.github.com>
2024-09-24 17:33:35 +0530

    Update Footer.tsx

--- src/components/chat/Footer.tsx
+++ src/components/chat/Footer.tsx
@@ -42,7 +42,7 @@ const ChatFooter: React.FC<ChatFooterProps> = ({
   return (
     <form style={{ width: "100%" }} onSubmit={sendMessage}>
       <Flex
-        bottom="-5px"
+        bottom="-10px"
         color="white"
         w="100%"
         bg={bg}
@@ -51,7 +51,7 @@ const ChatFooter: React.FC<ChatFooterProps> = ({
         alignItems="center"
         paddingLeft="30px"
         gap="10px"
-        
+        position="fixed"
       >
         <Input
           bg="white"
